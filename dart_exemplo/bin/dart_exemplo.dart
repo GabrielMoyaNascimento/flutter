35c4b3939c671743d71044eab1636d14f482a421
EduardoDfs <odraudesaiddfsgmail.com>
2022-04-26 16:53:41 -0300

    Mais alguns exercícios de exemplo

--- dart_exemplo/bin/dart_exemplo.dart
+++ dart_exemplo/bin/dart_exemplo.dart
@@ -2,48 +2,67 @@ import 'dart:io';
 import 'package:dart_exemplo/dart_exemplo.dart' as dart_exemplo;
 
 void main(List<String> arguments) {
-  print('\n-------------');
+  // print('\n-------------');
 
-  print('Hello Word');
+  // print('Hello Word');
 
-  print('\n----String-----\n');
+  // print('\n----String-----\n');
 
-  String texto;
-  texto = "Seu texto aqui";
-  print("Olha aqui:" + texto);
+  // String texto;
+  // texto = "Seu texto aqui";
+  // print("Olha aqui:" + texto);
 
-  print('\n----Input-----');
+  // print('\n----Input-----');
 
-  print('\nDigite um texto');
-  String? valor = stdin.readLineSync();
-  print('\nResposta: $valor');
+  // print('\nDigite um texto');
+  // String? valor = stdin.readLineSync();
+  // print('\nResposta: $valor');
 
-  print('\n---Calculadora----');
+  // print('\n---Calculadora----');
 
-  print('\nDigite o primeiro valor:');
-  double? num1 = double.parse(stdin.readLineSync()!);
-  print('\nDigite o segundo valor:');
-  double? num2 = double.parse(stdin.readLineSync()!);
+  // print('\nDigite o primeiro valor:');
+  // double? num1 = double.parse(stdin.readLineSync()!);
+  // print('\nDigite o segundo valor:');
+  // double? num2 = double.parse(stdin.readLineSync()!);
 
-  double soma = somar(num1, num2);
-  double multiplicar = mult(num1, num2);
-  double subtrair = sub(num1, num2);
-  double dividir = divi(num1, num2);
+  // double soma = somar(num1, num2);
+  // double multiplicar = mult(num1, num2);
+  // double subtrair = sub(num1, num2);
+  // double dividir = divi(num1, num2);
 
-  print("A soma dos valores é: $soma");
-  print("A multiplicação dos valores é: $multiplicar");
-  print("A subtração dos valores é: $subtrair");
-  print("A divisão dos valores é: $dividir");
+  // print("A soma dos valores é: $soma");
+  // print("A multiplicação dos valores é: $multiplicar");
+  // print("A subtração dos valores é: $subtrair");
+  // print("A divisão dos valores é: $dividir");
 
-  print('\n----Verificar-Idade-----');
+  // print('\n----Verificar-Idade-----');
 
-  print('\nDigite sua idade:');
-  int? idade = int.parse(stdin.readLineSync()!);
-  String resultadoIdade = verificarIdade(idade);
-  print(resultadoIdade);
+  // print('\nDigite sua idade:');
+  // int? idade = int.parse(stdin.readLineSync()!);
+  // String resultadoIdade = verificarIdade(idade);
+  // print(resultadoIdade);
 
-  print('\n-------------');
+  // print('\n-----Maior-Valor-----');
 
+  // print('\nDigite o primeiro número:');
+  // double? num3 = double.parse(stdin.readLineSync()!);
+  // print('\nDigite o segundo número:');
+  // double? num4 = double.parse(stdin.readLineSync()!);
+  // String resultadoMaiorValor = maiorValor(num3, num4);
+  // print(resultadoMaiorValor);
+
+  print('\n-----Definir-Média-Notas-----');
+
+  print('\nDigite a primeira nota:');
+  double? nota1 = double.parse(stdin.readLineSync()!);
+  print('\nDigite a segunda nota:');
+  double? nota2 = double.parse(stdin.readLineSync()!);
+  print('\nDigite a terceira nota:');
+  double? nota3 = double.parse(stdin.readLineSync()!);
+  String resultadoMediaNotas = verificarMediaGeral(nota1, nota2, nota3);
+  print(resultadoMediaNotas);
+
+  print('\n----------');
 }
 
 somar(num1, num2) {
@@ -75,5 +94,28 @@ verificarIdade(idade) {
     return "Você é um adulto/a";
   } else {
     return "Você é um idoso/a";
+  }
+}
+
+maiorValor(num1, num2) {
+  if (num1 > num2) {
+    return "O primeiro número é maior";
+  } else if (num2 > num1) {
+    return "O segundo número é maior";
+  } else {
+    return "Os números são iguais";
+  }
+}
+
+verificarMediaGeral(nota1, nota2, nota3) {
+  double media = (nota1 + nota2 + nota3) / 3;
+  if (media < 60) {
+    return "Sua média final foi D";
+  } else if (media >= 60 && media < 75) {
+    return "Sua média final foi C";
+  } else if (media >= 75 && media < 85) {
+    return "Sua média final foi B";
+  } else {
+    return "Sua média final foi A";
   }
 }
